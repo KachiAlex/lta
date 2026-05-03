ab6dd8ca0b39f58d571d2d91fe8e3d258fa01d9b
Cleanjohn Dev <dev@cleanjohn.com>
2026-05-03 18:36:01 +0100

    Set Vite root to src

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({});
+export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+});
